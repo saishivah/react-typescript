9cb66cf8bdc21932c386c769d07c0aff8110b644
sai shiva hari prasad <esshariprasad@gmail.com>
2024-02-19 20:35:12 -0600

    added array type to reacttsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
 import { Greet } from './Greet';
+import { Person } from './Person';
+import { PersonList } from './PersonList';
 
 function App() {
+  
+  const personName={
+    first:'Bruce',
+    last:'Wayne',
+  }
+  const nameList = [
+    {
+      first:'Bruce',
+      last:'Wayne'
+  },
+  {
+    first:'Clark',
+    last:'Kent',
+  },
+  {
+    first:'Princess',
+    last:'Diana',
+  },
+]
+
+
   return (
     <div className="App">
   
-     <Greet name="Hari"/>
+     <Greet name="Hari" messageCount={20} isloggedIn={false}/>
+     <Person name={personName}/>
+     <PersonList names={nameList}/>
+
 
     </div>
   );
--- src/Greet.tsx
+++ src/Greet.tsx
@@ -1,11 +1,17 @@
 type GreetProps={
-    name:string;    
+    name:string,
+    messageCount:number ,
+    isloggedIn:boolean
 }
 
 export const Greet = (props:GreetProps)=>{
     return(
         <div>
-           <h2>Welcome to {props.name}</h2> 
+        <h2>  {props.isloggedIn?`Welcome to ${props.name}. You have ${props.messageCount} messages`
+        :'Welcome Guest'
+        }
+
+      </h2> 
         </div>
     )
 }
